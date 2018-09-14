4159af8b771b0679f465141344b3d4da93d6614a
Clementine URQUIZAR <curquiza@e2r4p23.42.fr>
2018-09-14 10:30:05 +0200

    upd main script

--- start_init.sh
+++ start_init.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
 CONFIG_DIR='config_files'
+FISH_FILE=$HOME/.config/fish/config.fish
+ZSH_FILE=$HOME/.zshrc
+ALIAS_FILE=$CONFIG_DIR/aliases.txt
 
 # creating Documents folder
 mkdir -p $HOME/Documents/
 
-# configuring zsh
+# installing oh-my-zsh and configuring zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cat $CONFIG_DIR/.zshrc >> $HOME/.zshrc
+echo "\nAliases :" >> $ZSH_FILE
+cat $ALIAS_FILE >> $ZSH_FILE && source $ZSH_FILE
 
-# copying config files
+# configuring fish shell
+echo "\nAliases :" >> $FISH_FILE
+cat $ALIAS_FILE >> $FISH_FILE
+sed -i -e 's/=/ /g' $FILE_FILE
+rm -f "$FILE_FILE-e"
+source $FISH_FILE
+
+# configuring vim
 cp $CONFIG_DIR/.vimrc $HOME
-cp $CONFIG_DIR/.zshrc $HOME
-cp $CONFIG_DIR/config.fish $HOME/.config/fish
 
 # brew
 rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew
-echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc
-echo 'env PATH=$HOME/.brew/bin:$PATH' >> $HOME/.config/fish/config.fish && source $HOME/.config/fish/config.fish
+echo "\nBrew env var :" | tee $ZSH_FILE $FISH_FILE
+echo 'export PATH=$HOME/.brew/bin:$PATH' >> $ZSH_FILE && source $ZSH_FILE
+echo 'env PATH=$HOME/.brew/bin:$PATH' >> $FISH_FILE && source $FISH_FILE
 brew update
 
 # installing packages
