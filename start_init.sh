7ea5e3cf13cd0a0f746c6a2816760c2e7872b0d3
curquiza <clementine.urquizar@gmail.com>
2018-09-14 14:29:04 +0200

    fix errors

--- start_init.sh
+++ start_init.sh
@@ -10,25 +10,29 @@ ZSH_CONFIG=$HOME/.zshrc
 GIT_CONFIG=$HOME/.gitconfig
 
 # creating Documents folder
+echo "Creating Documents folder..."
 mkdir -p $HOME/Documents/
 
 # installing oh-my-zsh
 #sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # configuring zsh
-echo "\nAliases :" >> $ZSH_CONFIG
+echo "Configuring zsh..."
+echo "\n# Aliases :" >> $ZSH_CONFIG
 cat $ALIAS_FILE >> $ZSH_CONFIG
-echo "\nEnv var :" >> $ZSH_CONFIG
+echo "\n# Env var :" >> $ZSH_CONFIG
 cat $ENV_VAR_FILE >> $ZSH_CONFIG
 source $ZSH_CONFIG
 
 # brew
+echo "Installing and updating brew..."
 rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew
 echo "\nBrew env var :" | tee $ZSH_CONFIG $FISH_CONFIG
 echo 'export PATH=$HOME/.brew/bin:$PATH' >> $ZSH_CONFIG && source $ZSH_CONFIG
-echo 'env PATH=$HOME/.brew/bin:$PATH' >> $FISH_CONFIG && source $FISH_CONFIG
+echo 'set -x PATH $HOME/.brew/bin:$PATH' >> $FISH_CONFIG && source $FISH_CONFIG
 brew update
 
 # installing fish shell and configuring
+echo "Installing and configuring fish shell..."
 brew install fish
 # aliases
 echo "\nAliases :" >> $FISH_CONFIG
@@ -44,13 +48,18 @@ rm -f "$FILE_FILE-e"
 source $FISH_CONFIG
 
 # vim
+echo "Configuring vim..."
 cp $CONFIG_DIR/.vimrc $HOME
 
 # git
+echo "Configuring git..."
 cp $CONFIG_DIR/.gitconfig $HOME
 git config --global user.name "curquiza"
 git config --global user.email clementine.urquizar@gmail.com
 
 # installing packages
-brew install docker-machine
-brew install htop
+echo "Installing other packages :"
+#echo "docker machine..."
+#brew install docker-machine
+#echo "htop..."
+#brew install htop
